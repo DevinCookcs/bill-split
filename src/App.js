e0a047eeb9dfb1c49eb71f4ffa0887722830452b
Devin Cook <144957285+DevinCookcs@users.noreply.github.com>
2023-12-01 11:59:30 -0700

    converted form elements to controlled elements and calculated friendsexpense

--- src/App.js
+++ src/App.js
@@ -150,21 +150,38 @@ function FormAddFriend({ onAddFriends }) {
 }
 
 function FormSplitBill({ selectedFriend }) {
+  const [bill, setBill] = useState("");
+  const [userExpense, setUserExpense] = useState("");
+  const friendExpense = bill ? bill - userExpense : "";
+  const [whoPaid, setWhoPaid] = useState("user");
+
   return (
     <form className="form-split-bill">
       <h2>Split a bill with {selectedFriend.name}</h2>
 
       <label>💰 Bill value </label>
-      <input type="text" />
+      <input
+        type="text"
+        value={bill}
+        onChange={(e) => setBill(Number(e.target.value))}
+      />
 
       <label>👱 Your expense</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={userExpense}
+        onChange={(e) =>
+          setUserExpense(
+            Number(e.target.value) > bill ? userExpense : Number(e.target.value)
+          )
+        }
+      />
 
       <label>🫂 {selectedFriend.name}'s expense</label>
-      <input type="text" disabled />
+      <input type="text" disabled value={friendExpense} />
 
       <label>Who is paying the bill</label>
-      <select>
+      <select value={whoPaid} onChange={(e) => setWhoPaid(e.target.value)}>
         <option value="user">You</option>
         <option value="friend">{selectedFriend.name}</option>
       </select>
